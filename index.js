c37c7698a0d2c7fc9014340228c2341cbeed6d5d
Munkhtushig <munkhtushig9911@lab-1-02s-iMac.local>
2022-12-15 19:40:58 +0800

    ncie

--- index.js
+++ index.js
@@ -3,29 +3,96 @@ import express from "express";
 const port = 3000;
 const app = express();
 
+app.use(express.json());
+
+const users = [
+  {
+    id: 1,
+    username: "John",
+    email: "john@example.com",
+    password: "123",
+  },
+  {
+    id: 2,
+    username: "Done",
+    email: "done@example.com",
+    password: "123",
+  },
+];
+
+const posts = [];
+
 app.get("/", (req, res) => {
-  res.send(`
+  res.send({
+    data: users,
+  });
+});
+
+app.post("/signup", (request, response) => {
+  const { username, email, password, age, gender } = request.body;
+  const user = {
+    id: users.length + 1,
+    username,
+    email,
+    password,
+    age,
+    gender,
+  };
+  users.push(user);
+  response.send({
+    success: true,
+    msg: "Your account has been created"
+  })
+
+});
+
+app.post("/posts", (req, res) => {
+  const { title, description} = req.body;
+  const post = {
+    id: posts.length + 1,
+    title,
+    description,
+  };
+  posts.push(post);
+  res.send({
+    success: true,
+    msg: "Post has been uploaded successfully"
+  })
+});
+
+app.get("/posts", (req, res) => {
+  posts.map((post) => {
+    res.send(`
     <!DOCTYPE html>
     <html lang="en">
       <head>
         server response
       <head/>
-    <body style="border:1px solid blue">
-        <h1>home page um</h1>        
-        <h2>lockdownbawnsha</h2>
-        <div>
-            <span>
-                hair um ansi nawjdn kasjdnk ajdsnfk jabdflkjb alskjdbf lkasdjb laksjdbn lkajsnd
-            </span>
-        </div>
-    </body>   
-  `);
+      <body>
+          <h1>${post.title}</h1> 
+          <div>
+              <span>
+                  ${post.description}
+              </span>
+          </div>
+      </body>   
+    `)
+  })
 });
 
-app.get("/api", (req, res) => {
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  users.map((user) => {
+    if (user.username === username && user.password === password) {
+      res.send({
+        success: true,
+        msg: "Ta amjjilttai newterlee",
+      });
+    }
+  });
   res.send({
-    success: true,
-    data: `Hello ${req.originalUrl}`,
+    success: false,
+    msg: "Newtreh ner, nuuts ug buruu",
   });
 });
 
