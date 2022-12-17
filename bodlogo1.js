bf3c7d002ab80aaeef954172553f2c583d87545e
Munkhtushig <munkhtushig9911@lab-1-02s-iMac.local>
2022-12-17 14:03:14 +0800

    dad

--- bodlogo1.js
+++ bodlogo1.js
@@ -88,16 +88,78 @@
 
 // =-12=3-1=2-3=1-2=3-1=23=-12=3-=12-3=1-2=3-1=23-=1-23=-1=2-3=1-23=1-2=3-1=2-3=1-2=3-1=2-3
 
-const mySort = (array) => {
-  for (let i = 0; i < array.length; i++) {
-    let target = array[i];
-    for (var j = i - 1; j >= 0 && array[j] > target; j--) {
-      array[j + 1] = array[j];
+// const mySort = (array) => {
+//   for (let i = 0; i < array.length; i++) {
+//     let target = array[i];
+//     for (var j = i - 1; j >= 0 && array[j] > target; j--) {
+//       array[j + 1] = array[j];
+//     }
+//     array[j + 1] = target;
+//   }
+//   console.log(array);
+// };
+
+// mySort([12, 23, 53, 1, 14, 16, 90])
+
+// const nums = [1, 2, 6, 2, 5];
+// const target = 3;
+
+// const sum = () => {
+//   nums.map((el, i) => {
+//     nums.map((el2, j) => {
+//       if (el + el2 ===   target) {
+//         console.log(`${el} + ${el2} = ${target}`);
+//       }
+//     });
+//   });
+// };
+
+// sum();
+
+// const target = 16;
+
+// const sum = (array) => {
+//   array.filter ((el) => {
+//     console.log( el === target);
+//   })
+// };
+
+// sum([1, 23, 123, 12, 432, 4]);
+
+// 1217nii bodlogos
+
+const array = [1, 5, 7, 3, 1, 9, 6, 12];
+let sum = 0;
+let target = 10;
+let notDavhtsah = 0;
+//1. array dahi element-uudin niilber olooroi
+// array.map((el) => {
+//   sum += el;
+// })
+// console.log(sum);
+
+//2. array dahi 5-s ih elementud olooroi
+// array.map((el) => {
+//   if( el > 5) {
+//     console.log(`5-aas ih toonuud : ${el}`);
+//   }
+// })
+
+//3. array-g ihees bagaruu sort hiigeerei
+// array.sort(function(a, b){return b - a});
+// console.log(array);
+
+//4, array-s niilber 10 baih 2 toog ol
+// for (let i = 0; i < array.length; i++) {
+  
+// }
+
+//5. array dahi sondgoi toonuudig ustga
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array.length; j++) {
+    if (array[j] % 2 === 1) {
+      array.splice(j, 1);
     }
-    array[j + 1] = target;
   }
-  console.log(array);
-};
-
-mySort([12, 23, 53, 1, 14, 16, 90]);
-
+}
+console.log(array);
